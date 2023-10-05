692df08bb4d3c7b2de1c4efa14871b2f855c57ed
Shenya De Silva <sde-silv@c4c3c5.42berlin.de>
2023-10-05 18:39:09 +0200

    Included changed to pushswap files

--- push_swap.c
+++ push_swap.c
@@ -1,38 +1,35 @@
-/*
 #include <unistd.h>
 #include <stdlib.h>
 
-
-int ft_strlen(char  *str)
+int	ft_strlen(char  *str)
 {
-    int len;
+	int len;
 
-    len = 0;
-    if (!str)
-        return (len);
-    while (str[len] != '\0')
-        len ++;
-    return (len);
+	len = 0;
+	if (!str)
+		return (len);
+	while (str[len] != '\0')
+		len ++;
+	return (len);
 }
-*/
-#include "push_swap.h"
 
 int main(int argc, char **argv)
 {
-    int i;
+	int i;
 
-    i = 2;
-    if (argc == 2)
-        write (1, argv[1], ft_strlen(argv[1]));
-    else if (argc > 2)
-    {
-        while (i <= argc)
-        {
-            write (1, argv[i - 1], ft_strlen(argv[i - 1]));
-            write (1, " ", 1);
-            i ++;
-        }
-    }
-    else
-        write (1, "\n", 1);
+	i = 2;
+	if (argc == 2)
+		write (1, argv[1], ft_strlen(argv[1]));
+	else if (argc > 2)
+	{
+		while (i <= argc)
+		{
+			write (1, argv[i - 1], ft_strlen(argv[i - 1]));
+			if (i < argc)
+				write (1, " ", 1);
+			i ++;
+		}
+	}
+	else
+		write (1, "\n", 1);
 }
--- push_swap.h
+++ push_swap.h
@@ -1,7 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 18:52:34 by sde-silv          #+#    #+#             */
+/*   Updated: 2023/10/04 18:52:47 by sde-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "libft/libft.h"
-
+typedef struct node
+{
+	int				content;
+	int				index;
+	struct node		*next;
+}					t_stack;
 
 #endif
