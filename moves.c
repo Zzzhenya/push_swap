27cff7f2def9c1adbb6c381b03d95f0edf72e50f
Shenya De Silva <sde-silv@c4c1c3.42berlin.de>
2023-10-10 16:42:53 +0200

    Initial commit for push swap moves

---
+++ moves.c
@@ -0,0 +1,36 @@
+#include "push_swap.h"
+
+int swap(char c, t_stack *ptr)
+{
+
+}
+
+int push(char c, t_stack *ptr)
+{
+
+}
+
+int rot(char c, t_stack *ptr)
+{
+
+}
+
+int rev_rot(char c, t_stack *ptr)
+{
+
+}
+
+int swap_both(t_stack *a, t_stack *b)
+{
+
+}
+
+int rot_both(t_stack *a, t_stack *b)
+{
+
+}
+
+int rev_rot_both(t_stack *a, t_stack *b)
+{
+
+}
