5612ec7f3149a988f901553f80bbcbd213a163a7
Shenya De Silva <shenya44@outlook.com>
2023-10-13 00:04:49 +0200

    Relocated main function from moves

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "push_swap.h"
+
+int main(void)
+{
+    t_stack **a;
+    int     i;
+
+    i = 1;
+    a = malloc(sizeof(t_stack **));
+    if (!a)
+        return (0);
+    *a = ft_stknew(i);
+    i ++;
+    while (i < 4)
+    {
+        ft_stkadd_front(a, ft_stknew(i));
+        i ++;
+    } 
+    print_stack(*a);
+    clear_stack(a);
+    a = NULL;
+    free(a);
+    return (0);
+}
