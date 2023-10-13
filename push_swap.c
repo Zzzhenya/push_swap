8eedc29cacb81922c44bf1f929aaf4c82827cc10
Shenya De Silva <shenya44@outlook.com>
2023-10-13 13:28:34 +0200

    Removed push swap main

--- push_swap.c
+++ push_swap.c
@@ -58,7 +58,7 @@ int	ft_parse(char **argv)
 	return (0);
 }
 
-//t_stack	store_in_stack(t_stack **a, char **argv, int c)
+//t_stack	store_in_stack(t_stack **a, char **argv, int argc)
 void store_in_stack(char **argv, int argc)
 {
 	int i;
@@ -82,41 +82,3 @@ void store_in_stack(char **argv, int argc)
 		}
 	}
 }
-
-/*
-int	main(int argc, char **argv)
-{
-	t_stack	**a;
-	//t_list	**b;	
-
-	//if (argc == 2)
-	//	write (1, argv[1], ft_strlen(argv[1]));
-	//else if (argc > 2)
-	
-	if (argc < 2)
-		return (0);
-		//write (1, "Error\n", 6);
-	else if (argc == 2)
-	{
-		if (ft_parse(ft_split(argv[1], ' ')) < 0)
-			write (1, "Error\n", 6);
-		else
-		{
-			//a = store_in_stack(a, argv, argc);
-			//store_in_stack(ft_split(argv[1]), argc);
-			printf("Yet to implement\n");
-		}
-	}
-	else //(argc > 2)
-	{
-		if (ft_parse(argv) < 0)
-			write (1, "Error\n", 6);
-		else
-		{
-			//a = store_in_stack(a, argv, argc);
-			store_in_stack(argv, argc);
-		}
-	}
-	return (0);
-}
-*/
