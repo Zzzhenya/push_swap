e308145c21734c7eafa4223ce61c3928421c4f61
Shenya De Silva <shenya44@outlook.com>
2023-10-17 22:31:23 +0200

    Radix sort first step

--- radix_sort.c
+++ radix_sort.c
@@ -1,83 +1,46 @@
-#include <stdio.h>
-int getMax(int arr[], int n) 
-{ 
-    int mx = arr[0]; 
-    for (int i = 1; i < n; i++) 
-        if (arr[i] > mx) 
-            mx = arr[i]; 
-    return mx; 
-} 
-  
-// A function to do counting sort of arr[] 
-// according to the digit 
-// represented by exp. 
-void countSort(int arr[], int n, int exp) 
-{ 
-  
-    // Output array 
-    int output[n]; 
-    int i, count[10] = { 0 }; 
-  
-    // Store count of occurrences 
-    // in count[] 
-    for (i = 0; i < n; i++) 
-        count[(arr[i] / exp) % 10]++; 
-  
-    // Change count[i] so that count[i] 
-    // now contains actual position 
-    // of this digit in output[] 
-    for (i = 1; i < 10; i++) 
-        count[i] += count[i - 1]; 
-  
-    // Build the output array 
-    for (i = n - 1; i >= 0; i--) { 
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i]; 
-        count[(arr[i] / exp) % 10]--; 
-    } 
-  
-    // Copy the output array to arr[], 
-    // so that arr[] now contains sorted 
-    // numbers according to current digit 
-    for (i = 0; i < n; i++) 
-        arr[i] = output[i]; 
-} 
-  
-// The main function to that sorts arr[] 
-// of size n using Radix Sort 
-void radixsort(int arr[], int n) 
-{ 
-  
-    // Find the maximum number to 
-    // know number of digits 
-    int m = getMax(arr, n); 
-  
-    // Do counting sort for every digit. 
-    // Note that instead of passing digit 
-    // number, exp is passed. exp is 10^i 
-    // where i is current digit number 
-    /*for (int exp = 1; m / exp > 0; exp *= 10) 
-        countSort(arr, n, exp);*/
-    int exp = 1;
-    while (m / exp > 0)
+#include "push_swap.h"
+
+/* max nval*/
+int get_max(t_stack *stack)
+{
+    int max;
+    t_stack *current;
+
+    if (!stack)
+        return (0);
+    current = stack;
+    max = current->nval;
+    while (current)
     {
-        countSort(arr, n, exp);
-        exp = exp * 10;
-    } 
-} 
-  
-// Driver Code 
-int main() 
-{ 
-    int arr[] = { 543, 986, 217, 765, 329 }; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-  
-    // Function Call 
-    radixsort(arr, n); 
-    int i = 0;
-    while (i<n)
-    {
-        printf("%d: %d\n", i , arr[i]);
-        i ++;
+        if (current->nval > max)
+            max = current->nval;
+        current=current->next;
     }
-    return 0; 
+    return (max);
 }
+
+void ft_radix_sort(t_stack **a, t_stack **b)
+{
+    int size_a;
+    t_stack *this;
+    //int max;
+
+    size_a = ft_stksize(*a);
+    this = *a;
+    //max = get_max(*a);
+    while (size_a > 0)
+    {
+        if (((this->nval >> 1)& 1) == 1)
+            rot('a', a);
+        else
+            push('b', a, b);
+        this = *a;
+        size_a --;
+    }
+}
+/*
+while (this && size_a > 0)
+    {
+        if (this->)
+    }
+*/
