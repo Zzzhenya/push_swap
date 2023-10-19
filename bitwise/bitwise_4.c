e0acdf5bb8c87cbd1d617e69e38860554f4e8ae2
Shenya De Silva <sde-silv@c4c4c4.42berlin.de>
2023-10-19 17:57:48 +0200

    Change index to print the numbers pushed to b

--- bitwise/bitwise_4.c
+++ bitwise/bitwise_4.c
@@ -6,34 +6,62 @@
 int ft_len(int tab[])
 {
     int i = 0;
-    if (!*tab)
+    if (!tab[i])
         return (i);
     while (tab[i])
         i ++;
     return (i);
 }
 
-void    ft_bin(int tab[], int len)
+void    ft_push_one(int a[], int b[])
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    int index = 2;
+    while (a[i])
+    {
+        if (((a[i] >> index ) & 1) == 0)
+        {
+            b[j] = a[i];
+            a[i] = -1;
+            j ++;
+        }
+         i ++;
+    }
+}
+
+void    ft_print(int tab[], int len)
 {
     int i;
 
     i = 0;
+    if (!tab[i])
+        exit(printf("Empty\n"));
     while (i < len)
     {
-        printf(" %d :", tab[i]);
+        printf(" %d ", tab[i]);
         i ++;
-        printf("\n");
     }
+    printf("\n");
 }
 
 int main (void)
 {
     int len;
-    int num[7]= { 4, 3, 6, 2, 7, 1, 5};
+    int a[7]= { 4, 3, 6, 2, 7, 1, 5};
+    int b[7] = {0, 0, 0, 0, 0, 0, 0};
 
-    len = ft_len(num);
-    printf("len: %d\n", len);
-    ft_bin(num, len);
+    printf("len: %d\n", ft_len(a));
+    printf("a :");
+    ft_print(a, ft_len(a));
+    ft_push_one(a, b);
+    printf("a :");
+    ft_print(a, ft_len(a));
+    printf("b :");
+    ft_print(b, ft_len(b));
 
     return (0);
 }
