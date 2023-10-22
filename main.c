dd17d0749115a970ea8bfef2494dd1e04d76363f
Shenya De Silva <shenya44@outlook.com>
2023-10-22 20:22:25 +0200

    combined argc = 2 and argc > 2 functions

--- main.c
+++ main.c
@@ -26,95 +26,67 @@ int max_bin_digits(t_stack *stack)
     return (n);
 }
 
-int nums_as_args(char **argv)
+int ft_pushswap(t_stack **a, t_stack **b)
 {
-    t_stack    **a;
-    t_stack    **b;
     int         max;
 
-    a = NULL;
-    if (ft_parse(argv, 1) < 0)
-    {
-        write (1, "Error\n", 6);
-        return (0);
-    }
-    else
-    {
-        a = store_in_stack(a, argv, 1);
-        if (ft_issorted(*a))
-        {
-            clear_stack(a);
-            return (0);
-        }
-        b = malloc(sizeof(t_stack));
-        if (!b)
-        {
-            clear_stack(a);
-            return (0);
-        }
-        normalize(a);
-        max = max_bin_digits(*a);
-        ft_radix_sort(a, b, max);
-        clear_stack(a);
-        clear_stack(b);
-    }
+    normalize(a);
+    max = max_bin_digits(*a);
+    ft_radix_sort(a, b, max);
+    clear_stack(a);
+    clear_stack(b);
     return (1);
 }
 
-int nums_as_str(char *str)
+int ft_init(char **argv, int x)
 {
-    char       **arr;
     t_stack    **a;
     t_stack    **b;
-    int         max;
 
     a = NULL;
-    arr = NULL;
-    arr = ft_split(str, ' ');
-    if (!arr)
-        return (0);
-    if (ft_parse(arr, 0) < 0)
+    if (ft_parse(argv, x) < 0)
     {
-            // need to free the whole thing one by one
-            //free(arr);
         write (1, "Error\n", 6);
         return (0);
     }
-    else
+    a = store_in_stack(a, argv, x);
+    if (ft_issorted(*a))
     {
-        a = store_in_stack(a, arr, 0);
-        if (ft_issorted(*a))
-        {
-            clear_stack(a);
-            return (0);
-        }
-        b = malloc(sizeof(t_stack));
-        if (!b)
-            return (0);
-        normalize(a);
-        max = max_bin_digits(*a);
-        ft_radix_sort(a, b, max);
         clear_stack(a);
-        clear_stack(b);
-        return (1);
+        return (0);
     }
+    b = malloc(sizeof(t_stack ));
+    if (!b)
+    {
+        clear_stack(a);
+        return (0);
+    }
+    if (!ft_pushswap(a, b))
+        return (0);
+    return (1);
 }
 
 /*Main for Push Swap*/
 int main(int argc, char **argv)
 {
+    char **arr;
+
+    arr = NULL;
     if (argc < 2)
         return (0);
     else if (argc == 2)
     {
-        if (nums_as_str(argv[1]))
+        arr = ft_split(argv[1], ' ');
+        if (!arr || arr[0] == (void *)0)
+            return (0);
+        if (ft_init(arr, 0))
             return(0);
         else
             return (-1);
     }
     else
     {
-        if (nums_as_args(argv))
+        if (ft_init(argv, 1))
             return (0);
         else
             return (-1);
