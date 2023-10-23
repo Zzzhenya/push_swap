523ba679ef217dd1339ae1ecccf5c25c7f6aeeef
Shenya De Silva <shenya44@outlook.com>
2023-10-24 00:59:09 +0200

    Leaks for macos and valgrind for linux

--- tester.sh
+++ tester.sh
@@ -3,12 +3,34 @@
 make re -s
 echo "				make re"
 #./push_swap $@
-echo "				leak check"
-valgrind ./push_swap $@
-echo "							"
-ARG=$@; ./push_swap $ARG | ./checker* $ARG
-echo "				checker"
-./push_swap $@ | wc -l
-echo "				command count"
-make -s fclean
-echo "				make fclean"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	{
+		echo "				leak check"
+		valgrind ./push_swap $@
+		echo "							"
+		ARG=$@; ./push_swap $ARG | ./checker* $ARG
+		echo "				checker"
+		./push_swap $@ | wc -l
+		echo "				command count"
+		make -s fclean
+		echo "				make fclean"
+	}
+else
+	{
+		ARG=$@; ./push_swap $ARG | ./checker* $ARG
+		echo "				checker"
+		./push_swap $@ | wc -l
+		echo "				command count"
+		echo "							"
+		echo "				leak check"
+		export MallocStackLogging=1
+		leaks --atExit -- ./push_swap $@
+		echo "							"
+		echo "		please make fclean"
+		echo "							"
+	}
+fi
+
+
+
