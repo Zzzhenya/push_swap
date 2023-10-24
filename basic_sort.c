a7de4559a39c8bf1da92b6e5a16a57ad49551c16
Shenya De Silva <shenya44@outlook.com>
2023-10-24 18:21:55 +0200

    Sorting algorithms for size 2 - 5

--- basic_sort.c
+++ basic_sort.c
@@ -12,8 +12,88 @@
 
 #include "push_swap.h"
 
+static void	ft_sort_23(t_stack **a, int x)
+{
+	t_stack	*current;
+
+	current = *a;
+	if (x == 2)
+	{
+		if (current->nval > (current->next)->nval)
+			rot('a', a);
+	}
+	else
+	{
+		if (current->nval < (current->next)->nval)
+			rev_rot('a', a);
+		else if (current->nval == 3)
+			rot('a', a);
+		current = *a;
+		if (current->nval > (current->next)->nval)
+			swap('a', a);
+	}
+}
+
+static void	ft_sort_4(t_stack **a, t_stack **b)
+{
+	t_stack *current;
+
+	current = *a;
+	while (current->nval < 4)
+	{
+		rot ('a', a);
+		current = *a;
+	}
+	if (current->nval == 4)
+	{
+		push ('b', a, b);
+		if (!ft_issorted(*a))
+			ft_sort_23(a, 3);
+		push ('a', b, a);
+		rot ('a', a);
+	}
+}
+
+static void	ft_sort_5(t_stack **a, t_stack **b, int x)
+{
+	t_stack *current;
+
+	current = *a;
+	while (x > 0)
+	{
+		if (current->nval == 4 || current->nval == 5)
+			push ('b', a, b);
+		else
+			rot ('a', a);
+		current = *a;
+		x --;
+	}
+	if (!ft_issorted(*a))
+		ft_sort_23(a, 3);
+	if (!ft_issorted(*b))
+		ft_sort_23(b, 2);
+	push ('a', b, a);
+	rot ('a', a);
+	push ('a', b, a);
+	rot ('a', a);
+}
+
 void	basic_sort(t_stack **a, t_stack **b)
 {
-	if (*a || *b)
-		ft_printf("Basic_sort\n");
+	int size;
+
+	size = ft_stksize(*a);
+	if (*a)
+	{
+		if (size == 1)
+			return;
+		else if (size == 2)
+			ft_sort_23(a, 2);
+		else if (size == 3)
+			ft_sort_23(a, 3);
+		else if (size == 4)
+			ft_sort_4(a, b);
+		else
+			ft_sort_5(a, b, 5);
+	}
 }
