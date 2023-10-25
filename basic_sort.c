97294813d869741f7e42c0379f0256ec3721d8ee
Shenya De Silva <sde-silv@c4b8c6.42berlin.de>
2023-10-25 14:55:08 +0200

    Fixed 4 2 5 1 3 scenario

--- basic_sort.c
+++ basic_sort.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-static void	ft_sort_23(t_stack **a, int x)
+static void	ft_sort_23(t_stack **a, int x, int c)
 {
 	t_stack	*current;
 
@@ -20,17 +20,17 @@ static void	ft_sort_23(t_stack **a, int x)
 	if (x == 2)
 	{
 		if (current->nval > (current->next)->nval)
-			rot('a', a);
+			rot(c, a);
 	}
 	else
 	{
 		if (current->nval < (current->next)->nval)
-			rev_rot('a', a);
+			rev_rot(c, a);
 		else if (current->nval == 3)
-			rot('a', a);
+			rot(c, a);
 		current = *a;
 		if (current->nval > (current->next)->nval)
-			swap('a', a);
+			swap(c, a);
 	}
 }
 
@@ -48,7 +48,7 @@ static void	ft_sort_4(t_stack **a, t_stack **b)
 	{
 		push ('b', a, b);
 		if (!ft_issorted(*a))
-			ft_sort_23(a, 3);
+			ft_sort_23(a, 3, 'a');
 		push ('a', b, a);
 		rot ('a', a);
 	}
@@ -56,22 +56,26 @@ static void	ft_sort_4(t_stack **a, t_stack **b)
 
 static void	ft_sort_5(t_stack **a, t_stack **b, int x)
 {
-	t_stack	*current;
+	int		check;
 
-	current = *a;
+	check = 0;
 	while (x > 0)
 	{
-		if (current->nval == 4 || current->nval == 5)
+		if ((*a)->nval == 4 || (*a)->nval == 5)
+		{
 			push ('b', a, b);
+			check ++;
+			if (check == 2)
+				break ;
+		}
 		else
 			rot ('a', a);
-		current = *a;
 		x --;
 	}
 	if (!ft_issorted(*a))
-		ft_sort_23(a, 3);
+		ft_sort_23(a, 3, 'a');
 	if (!ft_issorted(*b))
-		ft_sort_23(b, 2);
+		ft_sort_23(b, 2, 'b');
 	push ('a', b, a);
 	rot ('a', a);
 	push ('a', b, a);
@@ -88,9 +92,9 @@ void	basic_sort(t_stack **a, t_stack **b)
 		if (size == 1)
 			exit (0);
 		else if (size == 2)
-			ft_sort_23(a, 2);
+			ft_sort_23(a, 2, 'a');
 		else if (size == 3)
-			ft_sort_23(a, 3);
+			ft_sort_23(a, 3, 'a');
 		else if (size == 4)
 			ft_sort_4(a, b);
 		else
