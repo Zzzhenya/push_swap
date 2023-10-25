cf233c57451933f52890b56a4a7d24ed101ea81f
Shenya De Silva <sde-silv@c4b8c6.42berlin.de>
2023-10-25 15:35:03 +0200

    Convert single line comments to multi line

--- push_swap.h
+++ push_swap.h
@@ -24,50 +24,50 @@ typedef struct node
 	struct node		*next;
 }					t_stack;
 
-//	debug utils
+/*	debug utils */
 
 void		print_stack(t_stack *stack);
 int			clear_stack(t_stack **stack);
 void		print_details(char c, t_stack *stack);
 int			free_arr(char **arr);
 
-//	stack utils
+/*	stack utils */
 
 void		ft_stkadd_front(t_stack **stack, t_stack *new);
 void		ft_stkadd_back(t_stack **stack, t_stack *new);
 t_stack		*ft_stknew(int content);
 int			ft_stksize(t_stack *stack);
 
-//	push_swap
+/*	push_swap */
 
 int			parse_for_dup(char **argv, int x);
 int			parse_for_type(char *str);
 int			ft_parse(char **argv, int x);
 t_stack		**store_in_stack(t_stack **a, char **argv, int i);
 
-//	moves
+/*	moves */
 
 void		swap(char c, t_stack **ptr);
 void		rot(char c, t_stack **ptr);
 void		rev_rot(char c, t_stack **ptr);
 void		push(char c, t_stack **x, t_stack **y);
 
-//	double moves
+/*	double moves */
 
 void		swap_both(t_stack **a, t_stack **b);
 void		rot_both(t_stack **a, t_stack **b);
 void		rev_rot_both(t_stack **a, t_stack **b);
 
-//	basic_sort
+/*	basic_sort */
 
 void		basic_sort(t_stack **a, t_stack **b);
 
-//	normalize
+/*	normalize */
 
 void		normalize(t_stack **stack);
 int			ft_issorted(t_stack *a);
 
-//	radix sort
+/*	radix sort */
 
 void		ft_radix_sort(t_stack **a, t_stack **b, int max);
 
