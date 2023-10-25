aef841d121e838015ab98d0087edf620e8110766
Shenya De Silva <sde-silv@c4b8c6.42berlin.de>
2023-10-25 15:24:58 +0200

    Removed ft_atoll

--- push_swap.h
+++ push_swap.h
@@ -30,7 +30,6 @@ void		print_stack(t_stack *stack);
 int			clear_stack(t_stack **stack);
 void		print_details(char c, t_stack *stack);
 int			free_arr(char **arr);
-long long	ft_atoll(const char *nptr);
 
 //	stack utils
 
