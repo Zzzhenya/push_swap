df6878b7e5c084d60e7f1ef681e72e488c6db5be
Shenya De Silva <sde-silv@c4b8c6.42berlin.de>
2023-10-25 15:23:52 +0200

    Removed local ft_atoll

--- debug_utils.c
+++ debug_utils.c
@@ -122,28 +122,3 @@ int main(void)
     return (0);
 }
 */
-
-long long	ft_atoll(const char *nptr)
-{
-	int			i;
-	long long	n;
-	int			sign;
-
-	i = 0;
-	sign = 1;
-	n = 0;
-	while (nptr[i] && ft_isspace(nptr[i]) != 0)
-		i ++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i ++;
-	}
-	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		n = (n * 10) + (nptr[i] - 48);
-		i ++;
-	}
-	return (sign * n);
-}
